c0bb00359917c8543d4f78cb874ed3d2a98e1fe0
Gilbert Tan <gilbert.tan1991@gmail.com>
2025-02-18 20:32:27 +0800

    Added the Dashboard including the sidebar

--- public/index.php
+++ public/index.php
@@ -1,5 +1,37 @@
 <?php
 
+
 require "../includes/db.php";
 
+
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Royalty - Dashboard</title>
+    <link rel="stylesheet" href="../public/assets/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Atkinson+Hyperlegible+Next:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
+
+</head>
+<body>
+
+<?php
+
+
+include "../includes/sidebar.php";
+include "../includes/header.php"; 
+
+?>
+
+
+    
+</body>
+</html>
+
+
